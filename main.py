250577168290cf863fb6028dec9aa31a0c127a41
Jeff Shively <jshively@paloaltonetworks.com>
2025-01-29 09:38:42 -0500

    initial push

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import requests
+import sys
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TSG_ID = os.environ.get("TSG_ID")
+CLIENT_ID = os.environ.get("CLIENT_ID")
+SECRET_ID = os.environ.get("SECRET_ID")
+
+
+def create_token():
+    url = f"https://auth.apps.paloaltonetworks.com/auth/v1/oauth2/access_token?grant_type=client_credentials&scope:tsg_id:{TSG_ID}"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json",
+    }
+    try:
+        token = requests.request(
+            "POST", url, headers=headers, auth=(CLIENT_ID, SECRET_ID)
+        ).json()
+        return token["access_token"]
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    token = create_token()
+    print(token)
