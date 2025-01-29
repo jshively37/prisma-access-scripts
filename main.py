288ab104f0add3f8f3d02eb68207641c3927c0b3
Jeff Shively <jshively@paloaltonetworks.com>
2025-01-29 11:05:31 -0500

    adding get security rule

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ TSG_ID = os.environ.get("TSG_ID")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 SECRET_ID = os.environ.get("SECRET_ID")
 
+BASE_URL = 'https://api.sase.paloaltonetworks.com/sse/config/v1'
+# HEADERS = {
+#   'Accept': 'application/json',
+# }
+
 
 def create_token():
     url = f"https://auth.apps.paloaltonetworks.com/auth/v1/oauth2/access_token?grant_type=client_credentials&scope:tsg_id:{TSG_ID}"
@@ -23,7 +28,17 @@ def create_token():
     except Exception as e:
         print(e)
 
+def get_security_rules():
+    url = f"{BASE_URL}/security-rules?position=pre&folder=Mobile%20Users"
+    HEADERS =   {
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    response = requests.get(url, headers=HEADERS).json()
+    print(response)
+
 
 if __name__ == "__main__":
     token = create_token()
-    print(token)
+    get_security_rules()
